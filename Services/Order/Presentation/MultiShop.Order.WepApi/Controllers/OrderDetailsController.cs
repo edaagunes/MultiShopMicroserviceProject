771455d4ce0c163ba0fd2ddd1f35414b32849d92
edaagunes <edagunes00@hotmail.com>
2025-05-24 20:05:19 +0300

    Added Identity configuration in microservices

--- Services/Order/Presentation/MultiShop.Order.WepApi/Controllers/OrderDetailsController.cs
+++ Services/Order/Presentation/MultiShop.Order.WepApi/Controllers/OrderDetailsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.Order.Core.Application.Features.CQRS.Commands.OrderDetailCommands;
 using MultiShop.Order.Core.Application.Features.CQRS.Handlers.OrderDetailHandlers;
@@ -6,6 +7,7 @@ using MultiShop.Order.Core.Application.Features.CQRS.Queries.OrderDetailQueries;
 
 namespace MultiShop.Order.WepApi.Controllers
 {
+	[Authorize]
 	[Route("api/[controller]")]
 	[ApiController]
 	public class OrderDetailsController : ControllerBase
